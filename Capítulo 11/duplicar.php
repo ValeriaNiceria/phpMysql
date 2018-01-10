7bd667ad37b956dcd7cabc01a5832ea4b8a5d02d
ValeriaNiceria <valerianiceria@gmail.com>
2018-01-10 12:44:37 -0200

    Capítulo 11 - arquivos antes de iniciar o estudo do capítulo

---
+++ Capítulo 11/duplicar.php
@@ -0,0 +1,10 @@
+<?php
+
+include "banco.php";
+
+$tarefa = buscar_tarefa($conexao, $_GET['id']);
+
+gravar_tarefa($conexao, $tarefa);
+
+header('Location: tarefas.php');
+die();
---
+++ Capítulo 11/tabela.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    
+    <table>
+        <tr>
+            <th>Tarefa</th>
+            <th>Descrição</th>
+            <th>Prazo</th>
+            <th>Prioridade</th>
+            <th>Concluída</th>
+            <th>Opções</th> <!--A nova coluna Opções-->
+        </tr>
+
+        <?php if(isset($lista_tarefas) && is_array($lista_tarefas) && sizeof($lista_tarefas) > 0) : ?>
+            <?php foreach($lista_tarefas as $tarefa) : ?>
+            
+                <tr>
+                    <td>
+                        <a href="tarefa.php?id=<?php echo $tarefa['id']; ?>">
+                            <?php echo isset($tarefa['nome']) ? $tarefa['nome'] : ''; ?>
+                        </a>
+
+                    </td>
+                    <td><?php echo isset($tarefa['descricao']) ? $tarefa['descricao'] : ''; ?></td>
+                    <td>
+                        <?php 
+                            echo traduz_data_para_exibir($tarefa['prazo']);
+                        ?>
+                    </td>
+                    <td>
+                        <?php 
+                        echo traduz_prioridade($tarefa['prioridade']);
+                        ?>
+                    </td>
+                    <td>
+                        <?php 
+                            echo traduz_concluida($tarefa['concluida']);
+                        ?>
+                    </td>
+                    <td> <!--O campo com os links para editar e remover-->
+                        <a href="editar.php?id=<?php echo $tarefa['id']; ?>" class="btn">
+                            Editar    
+                        </a>
+
+                        <a href="remover.php?id=<?php echo $tarefa['id']; ?>" class="btn">
+                            Remover
+                        </a>
+
+                        <a href="duplicar.php?id=<?php echo $tarefa['id']; ?>" class="btn">
+                            Duplicar tarefa
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif;?>
+        
+    </table>
+
+</body>
+</html>
